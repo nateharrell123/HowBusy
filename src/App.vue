e45ca4eb9fbb633357ef39398515c72d98224858
Nate Harrell <nateharrell@ksu.edu>
2021-09-24 16:22:16 -0500

    header styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Loader/>
+  <Header/>
   <Search/>
 </template>
 
 <script>
 import Loader from './components/Loader.vue'
 import Search from './components/Search.vue'
+import Header from './components/Header.vue'
 export default {
   name: 'App',
   components: {
     Loader,
-    Search
+    Header,
+    Search,
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="box-area">
-    <header>
-      <div class="wrapper">
+<header>
+    <div class="wrapper">
         <div class="logo">
-          <a href="#">Website Title</a>
+            <a href="#">Website Title</a>
         </div>
         <nav>
-          <a href="#">How it works</a>
+            <a href="#">How it works</a>
         </nav>
-      </div>
-    </header>
-  </div>
+    </div>
+</header>
 </template>
 
 <script>
@@ -19,12 +17,48 @@ export default {
 };
 </script>
 
-<style scoped>
-* {
+<style>
+*{
   margin: 0;
   padding: 0;
 }
-.wrapper {
-  width: 1170px;
+.logo{
+    width: 30%;
+    float: left;
+    line-height:100px;
 }
+.logo a{
+    text-decoration: none;
+    font-size: 30px;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color:#fff;
+    letter-spacing:5px;
+}
+.wrapper {
+  margin: 0 auto;
+}
+header{
+    height:100px;
+    background: #09A8D9;
+    width: 100%;
+    z-index: 12;
+    position:fixed;
+}
+body{
+    text-align: center;
+}
+nav{
+    float:right;
+    line-height: 100px;
+}
+nav a{
+    text-decoration: none;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    letter-spacing:4px;
+    font-size:20px;
+    margin:0 10px;
+    color:#fff;
+    padding-right: 50px;
+}
+
 </style>
