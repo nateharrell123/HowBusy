ac67f77613118686a10e7f61e9d0bed7c3d5bb95
Nate Harrell <nateharrell@ksu.edu>
2021-09-24 15:47:04 -0500

    working on header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box-area">
+    <header>
+      <div class="wrapper">
+        <div class="logo">
+          <a href="#">Website Title</a>
+        </div>
+        <nav>
+          <a href="#">How it works</a>
+        </nav>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.wrapper {
+  width: 1170px;
+}
+</style>
