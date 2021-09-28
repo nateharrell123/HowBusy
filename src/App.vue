3a0e300759a2fc10eae8d946648da63fa82f2812
Nate Harrell <nateharrell@ksu.edu>
2021-09-27 21:40:34 -0500

    working on table

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
   <Loader/>
-  <div class="document">
+  <div class="headerBotSpace">
     <Header/>
   </div>
+  <div class="searchBotSpace">
     <Search/>
+  </div>
+    <ResultsTable/>
 </template>
 
 <script>
 import Loader from './components/Loader.vue'
 import Search from './components/Search.vue'
 import Header from './components/Header.vue'
+import ResultsTable from './components/ResultsTable.vue'
 export default {
   name: 'App',
   components: {
     Loader,
     Header,
     Search,
+    ResultsTable
   }
 }
 </script>
 
 <style scoped>
 
-.document{
+.headerBotSpace{
   padding-bottom: 180px;
+}
+.searchBotSpace{
+  padding-bottom:180px;
 }
 
 </style>
---
+++ src/components/ResultsTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <table class="table">
+    <tr>
+        <th>Table Header 1</th>
+        <th>Table Header 2</th>
+        <th>Table Header 3</th>
+    </tr>
+    <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+        <td>Data 3</td>
+    </tr>
+    </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ResultsTable'
+}
+</script>
+
+<style scoped>
+.table{
+    width: 25%;
+    float: left;
+}
+</style>
