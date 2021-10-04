93a22f43c50e195b59d2491c8efb1045b5b2d3b7
Nate Harrell <nateharrell@ksu.edu>
2021-10-04 15:50:37 -0500

    follow tutorial

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,36 @@
     <Header />
   </div>
   <div class="push-right-lol">
-    <!--<div class="searchBotSpace">
-    </div> -->
-      <Search/>
+    <div class="searchBotSpace">
+      <!-- <Search/> -->
+      <Tutorial/>
+    </div> 
     <ResultsTable/>
   </div>
 </template>
 
 <script>
 import Loader from "./components/Loader.vue";
-import Search from "./components/Search.vue";
+//import Search from "./components/Search.vue";
 import Header from "./components/Header.vue";
+import Tutorial from "./components/Tutorial.vue"
 import ResultsTable from "./components/ResultsTable.vue";
 export default {
   name: "App",
   components: {
     Loader,
     Header,
-    Search,
+    //Search,
     ResultsTable,
+    Tutorial
   },
 };
 </script>
 
 <style scoped>
 .push-right-lol {
-  width:25%;
-  
+  margin-left:6%; 
+  /* mess with this */
 }
 
 .headerBotSpace {
---
+++ src/components/Tutorial.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="ui-grid">
+    <div class="six wide column">
+        <form class="ui segment large form">
+
+            <div class="field">
+                <div class="ui right icon input large">
+                    <input type="text" placeholder="Enter your address" />
+                    <i class="dot circle link icon"> </i>
+                </div>
+            </div>
+
+        </form>
+    </div>
+    <div class="ten wide column segment ui" ref="map"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Tutorial"
+}
+</script>
+
+<style scoped>
+
+</style>
