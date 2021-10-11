8c21a73065303adb15bc8daefc2882ce845dda5e
Nate Harrell <nateharrell@ksu.edu>
2021-10-11 17:04:12 -0500

    closer i think

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,22 +2,31 @@
     <div>
         <h1> Coordinates: </h1>
         <p> {{ coordinates.lat}} Latitude, {{ coordinates.lng}} Longitude </p>
+        <button @click="clickme"> Hey </button>
     </div>
 </template>
 
 <script>
 
-export default {
 
+export default {
     name: "Map",
     data() {
         return {
             coordinates: {
-                lat: 3,
-                lng: 2
+                lat: 0,
+                lng: 0
             }
         }
+    },
+
+    methods: {
+        clickme(){
+            console.log(this.$geolocation.coords)
+        }
     }
+
+
 }
 </script>
 
