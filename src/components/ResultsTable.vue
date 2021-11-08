f4a2a94c5120092d4d66fd5bcad05ca5ac89a294
Nate Harrell <nateharrell@ksu.edu>
2021-11-08 16:05:06 -0600

    styling

--- src/components/ResultsTable.vue
+++ src/components/ResultsTable.vue
@@ -3,17 +3,22 @@
     <table class="content-table">
       <thead>
         <tr>
-          <th>Table Header 1</th>
-          <th>Table Header 2</th>
-          <th>Table Header 3</th>
+          <th>Establishment</th>
+          <th></th>
+          <th>Busy-ness</th>
+          <th> Hours </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="place in SearchResults" :key="place.id">
-          <td>{{ place.name }}</td>
-          <td v-for="item in place.photos" :key="item.id"> {{ getPhoto(item.photo_reference) }} </td>
+          <td>{{ place.name }}
+            <br>
+            <span class="placeAddressText"> {{place.vicinity}}</span>
+          </td>
+          <!-- <td v-for="item in place.photos" :key="item.id"> {{ getPhoto(item.photo_reference) }} </td> -->
           <td> Picture goes here </td>
           <td>Data 3</td>
+          <td> </td>
         </tr>
       </tbody>
     </table>
@@ -56,8 +61,9 @@ export default {
 .content-table {
   border-collapse: collapse;
   margin: 25px 0;
-  font-size: 0.9em;
+  font-size: 1.3rem;
   min-width: 400px;
+  width:1000px;
   position: relative;
   justify-content: middle;
 
@@ -65,16 +71,25 @@ export default {
   overflow: hidden;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.25);
 }
+.placeAddressText{
+  color: rgba(0, 0, 0, 0.5);
+  font-size :1.0rem;
+}
 .content-table thead tr {
-  background-color: #09a8d9;
+  background-color: #22702A;
   color: #fff;
   text-align: left;
   font-weight: bold;
 }
 
-.content-table th,
+.content-table th{
+  padding: 12px 15px;
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+
+}
 .content-table td {
   padding: 12px 15px;
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
 }
 
 .content-table tbody tr {
