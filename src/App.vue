ca7fec3ee4f45882ebd993291b78c89ace5a82f3
Nate Harrell <nateharrell@ksu.edu>
2021-11-09 17:46:36 -0600

    adding a footer

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <Clock/>
   </div>
     <Search/>
+    <Footer/>
 </template>
 
 <script>
 import Clock from "./components/Clock.vue";
 import Search from "./components/Search.vue";
+import Footer from "./components/Footer.vue"
 
 export default {
   name: "App",
   components: {
     Clock,
-    Search
+    Search,
+    Footer
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="bottom">
+        <div class="links">
+
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name : "Footer"
+}
+</script>
+
+<style>
+.bottom{
+    background: #222;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 0 50px;
+    color: #aeb3ba;
+    flex-direction: column;
+}
+</style>
