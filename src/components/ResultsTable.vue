bc01389e30821b80cd865cc772714f7156208e39
Nate Harrell <nate.harrell@me.com>
2021-11-29 15:35:31 -0600

    working!!!

--- src/components/ResultsTable.vue
+++ src/components/ResultsTable.vue
@@ -69,26 +69,24 @@ export default {
     },
     howBusyClick(place_id){
        console.log(place_id)
-      // var axios = require("axios");
+      var axios = require("axios");
 
-      // var config = {
-      //   method: "post",
-      //   url: `http://127.0.0.1:5000/test`,
-      //   params: {
-      //     place : place_id
-      //   }
-      // };
-      // let self = this;
-      // axios(config)
-      //   .then(function (response) {
-      //     console.log(JSON.stringify(response.data))
-      //     self.busyNess = response.data
-      //   })
-      //   .catch(function (error) {
-      //     console.log(error);
-      //   });
-      return "hello"
-
+      var config = {
+        method: "post",
+        url: `http://127.0.0.1:5000/test`,
+        params: {
+          place : place_id
+        }
+      };
+      let self = this;
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data))
+          self.busyNess = response.data
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
     sort(s){
     if(s === this.currentSort) {
