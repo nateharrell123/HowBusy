f48fdbabd40c03dd90376742e37204aa55f819cf
Nate Harrell <nate.harrell@me.com>
2021-12-21 17:05:45 -0600

    sd

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Vue3Geolocation from 'vue3-geolocation';
-import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 const app = createApp(App);
 
 app.use(Vue3Geolocation)
-app.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo',
-        libraries: "places"
-    },
-}).mount('#app')
